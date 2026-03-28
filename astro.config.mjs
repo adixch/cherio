0a48ae1f9a6546019864e298c431ff87a5650fc9
Adrian Chalimoniuk <adrchalimoniuk@gmail.com>
2026-03-28 05:56:54 +0100

    fix: render blocking

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        inlineStylesheets: 'always',
+    },
     vite: {
         plugins: [tailwindcss()],
     },
